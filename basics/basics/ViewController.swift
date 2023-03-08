8042b2854c0b600f7b876c5282b3b3f5ca0acb90
JuajXavier <jvitorxavier@gmail.com>
2023-03-08 16:27:43 -0300

    Add dynamic table view

--- basics/basics/ViewController.swift
+++ basics/basics/ViewController.swift
@@ -7,7 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numbers.count;
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell();
+        
+        cell.textLabel?.text = String(numbers[indexPath.row]);
+        cell.textLabel?.textAlignment = .center;
+        
+        return cell;
+    }
+
 
     override func viewDidLoad() {
         
